1348fbac0c9109837abef1f7d4aee61a5267482e
Onkar Rane <onkarrane60@gmail.com>
2025-01-02 00:51:51 +0530

    MetaData Updated

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -5,21 +5,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';  // Import Bootstrap
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 
-// Manually write the HTML structure
-document.write(`
-  <!DOCTYPE html>
-  <html lang="en">
-  <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Real-Time Collaboration Tool</title>
-  </head>
-  <body>
-    <div id="root"></div>
-  </body>
-  </html>
-`);
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
